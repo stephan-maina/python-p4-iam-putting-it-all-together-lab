9633d34b23c6a8ea00c4124dcf1c76354d7a4d78
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-10-02 02:04:51 +0300

    completed...

--- server/models.py
+++ server/models.py
@@ -1,14 +1,16 @@
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-from config import db, bcrypt
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
-class User(db.Model, SerializerMixin):
-    __tablename__ = 'users'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
-    pass
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
-class Recipe(db.Model, SerializerMixin):
-    __tablename__ = 'recipes'
-    
-    pass
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
